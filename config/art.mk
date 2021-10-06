d181593d34d497ee29562242b9e538f9776bccb6
Trishiraj <gamerprince.exp@gmail.com>
2021-10-06 18:12:56 +0530

    vendor: Cleanup art config
    Change-Id: Ic61b95f143a501af45cda8b5013aa28e1dc04e22

--- config/art.mk
+++ config/art.mk
@@ -7,42 +7,15 @@ ART_BUILD_HOST_DEBUG := false
 USE_DEX2OAT_DEBUG := false
 
 # Dex pre-opt
-WITH_DEXPREOPT := true
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
 # Speed apps
 PRODUCT_DEXPREOPT_SPEED_APPS += \
   SystemUI \
   Settings \
-  Launcher3
+  CustomLauncher
 
 # GApps
 ifeq ($(USE_GAPPS),true)
 DONT_DEXPREOPT_PREBUILTS := true
 endif
-
-# Boot image profile
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
-# System server compiler
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
-# Dexopt boot types
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    pm.dexopt.first-boot=extract \
-    pm.dexopt.boot=verify
-
-# Dexopt filters
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    pm.dexopt.install=speed-profile \
-    pm.dexopt.bg-dexopt=speed-profile \
-    pm.dexopt.ab-ota=speed-profile \
-    pm.dexopt.inactive=verify \
-    pm.dexopt.shared=speed
-
-# dex2oat threads (default)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    dalvik.vm.dex2oat-threads=8 \
-    dalvik.vm.boot-dex2oat-threads=8
